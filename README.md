d6f8edc5e7ca86b21b163feba94b9106c7cb14d0
JonnyLatte <blackshadowknight@live.com>
2016-11-21 17:08:01 +1100

    update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # OptionMan
-Ethereum Option contract, one issuer many option holders 
+
+Ethereum Option contract, one issuer many option holders
+
+Contract owner can issue the right to buy at a fixed price before an expiry time an asset by locking the asset. 
+This right is represented as a standard token that can then be sold on exchanges.
+
+Before expiry:
+- Issuer can lock asset and receive options
+- Issuer can burn options to release asset
+- Issuer can withdraw excised funds (currency)
+- Option holder can pay currency to buy asset
+
+After expiry:
+- Option holder tokens cannot be used to by and so are worthless
+- Issuer can withdraw all funds from contract of any type
+
+
+Issues / thoughts:
+- Could incentivise a denial of service attack from the issier to the option holder to prevent exersise when un exersised options are deep in the money
+- Liquidity could be improved with multiple issuers This contract does not attempt to solve that problem
+- Some option sellers might prefer an option that can only be exersised at or close to expiry this contract does not attempt to solve that problem 
+- This current version has no events in the option code
+- This code is untested and likely contains errors. It is for demonstration purposes only. All included code requires audit or replacement with audited code. 
---
+++ contracts/optionMan.sol
@@ -0,0 +1,86 @@
+pragma solidity ^0.4.4;
+
+import "erc20.sol"; 
+import "owned.sol";
+import "ownedToken.sol";
+
+contract OptionMan is owned
+{
+    address public currency;   // Token used to buy / exsise
+    address public asset;      // Token on offer
+    uint256 public price;      // Amount of currency needed to buy a lot (smallest units)
+    uint256 public units;      // Amount of asset being sold in a lot (smallest units)
+    
+    uint256 public expireTime;  // trading ends at this timestamp
+    
+    address public options;
+
+    modifier onlyBeforeExpire() 
+    {
+        if(now < expireTime) _;
+        else throw;
+    }
+    
+    modifier onlyAfterExpire() 
+    {
+        if(now > expireTime) _;
+        else throw;
+    }
+    
+    function OptionMan(
+        address _currency, 
+        address _asset, 
+        uint256 _price, 
+        uint256 _units, 
+        uint256 _duration) 
+    {
+        currency = _currency;
+        asset = _asset;
+        price = _price;
+        units = _units;
+        expireTime = now + _duration;
+        options = new ownedToken();
+    }
+    
+    // seller locks asset and is given a token representing the option to buy it
+    
+    function issue(uint _value)  
+        onlyOwner
+        returns (bool ok)
+    {
+        if(!ERC20(asset).transferFrom(msg.sender, address(this),_value)) throw; 
+        if(!ownedToken(options).issue(_value,msg.sender)) throw;
+        return true;
+    }
+
+    // at any time owner can release funds by controlling the corrisponding option
+    // which will be burned.
+    
+    function burn(uint _value)  
+        onlyOwner
+        returns (bool ok)
+    {
+        if(!ownedToken(options).burn(_value,msg.sender)) throw;
+        if(!ERC20(asset).transfer(msg.sender,_value)) throw; 
+        return true;
+    }
+    
+     function withdraw(address _token, uint256 _value)
+        onlyOwner
+        onlyAfterExpire
+        returns (bool ok)
+    {
+        return ERC20(_token).transfer(msg.sender,_value);
+    }
+    
+    function exercise(uint256 _value) 
+        onlyBeforeExpire 
+    {
+        uint payment = _value * price / units;
+        if(!ownedToken(options).burn(_value,msg.sender)) throw;
+        if(!ERC20(currency).transferFrom(msg.sender, address(this),payment)) throw; 
+        if(!ERC20(asset).transfer(msg.sender,_value)) throw; 
+    } 
+    
+}
+
