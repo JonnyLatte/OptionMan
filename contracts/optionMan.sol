48e828f59e1bf725f8a9e9e1889a618e98a52d27
JonnyLatte <blackshadowknight@live.com>
2016-11-23 13:29:38 +1100

    tabs to spaces

--- contracts/optionMan.sol
+++ contracts/optionMan.sol
@@ -38,12 +38,12 @@ contract OptionMan is owned
         asset = _asset;
         price = _price;
         units = _units;
-        expireTime = now + _duration;		
-		
-		// token contract that is controlled by this contract 
-		// tokens can be issued or burned at any target address by the owner 
-		// or transfered normally by holders:
-		
+        expireTime = now + _duration;       
+        
+        // token contract that is controlled by this contract 
+        // tokens can be issued or burned at any target address by the owner 
+        // or transfered normally by holders:
+        
         options = new ownedToken(); 
 
     }
@@ -78,10 +78,10 @@ contract OptionMan is owned
     {
         return ERC20(_token).transfer(msg.sender,_value);
     }
-	
-	// currency does not need to be locked 
-	
-	function withdrawCurrency(uint256 _value)
+    
+    // currency does not need to be locked 
+    
+    function withdrawCurrency(uint256 _value)
         onlyOwner
         returns (bool ok)
     {
@@ -90,13 +90,13 @@ contract OptionMan is owned
     
     function exercise(uint256 _value) 
         onlyBeforeExpire
-        returns (bool ok)		
+        returns (bool ok)       
     {
         uint payment = _value * price / units;
         if(!ownedToken(options).burn(_value,msg.sender)) throw;
         if(!ERC20(currency).transferFrom(msg.sender, address(this),payment)) throw; 
         if(!ERC20(asset).transfer(msg.sender,_value)) throw; 
-		return true;
+        return true;
     } 
     
 }
